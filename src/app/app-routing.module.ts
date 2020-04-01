b7af6102af7a2f22bdcbf394a6edeffe12bbec47
Yukang Ken Zhang <yukang.ken.zhang@avanade.com>
2020-03-31 22:29:36 -0400

    check in new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CustomersComponent } from './customers/customers.component';
+import { CustomersGridComponent } from './customers-grid/customers-grid.component';
+import { CustomersEditComponent } from './customers-edit/customers-edit.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: CustomersComponent, pathMatch: 'full' },
+  { path: 'customers', component: CustomersComponent},
+  { path: 'customers/:id', component: CustomersEditComponent},
+  { path: '**', pathMatch: 'full', redirectTo: '/customers'}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomersGridComponent } from './customers-grid/customers-grid.component';
+import { CustomersEditComponent } from './customers-edit/customers-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent,
+    CustomersGridComponent,
+    CustomersEditComponent
   ],
   imports: [
     BrowserModule,
